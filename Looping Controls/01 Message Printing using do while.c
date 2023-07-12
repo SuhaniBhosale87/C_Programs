fca535873d684e468b5c2e6e13d001ec27939200
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-07-12 17:54:29 +0530

    Add files via upload

---
+++ Looping Controls/01 Message Printing using do while.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Cnt = 1;
+
+   printf("\n==========================*********==========================\n");
+
+    do
+    {
+            printf("\n\n Welcome");
+            printf("\n\a Hellow");
+            Cnt++;
+
+    }while(Cnt <= 5);
+
+
+    printf("\n==========================*********==========================\n");
+
+
+    printf("\n\n Thanks !!!");
+
+    getch();
+    return 0;
+}
