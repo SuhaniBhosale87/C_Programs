e49b971431ec737030994c107be1280e86212a51
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-07-12 18:02:09 +0530

    Add files via upload

---
+++ Basic/06 Using char Datatype.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch = 'A';
+
+        printf("\n Enter a Letter = ");
+
+        ch = getche();
+
+        printf("\n*****************=======********************");
+
+        printf("\n\n Welcome to Fork Infosystem !!!\n ");
+
+        printf("\n*****************=======********************");
+
+        printf("\n Value of Given Character = %c",ch);
+
+        getche();
+
+         getch();
+
+        return 0;
+}
