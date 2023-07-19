d0a8c95c582c7b70c32569222eef39f967401e2e
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-07-19 21:06:27 +0530

    Add files via upload

---
+++ Pattern Printing/13 Pattern - Right Lower Trangular.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int R = 0, C = 0 , i = 0;
+
+    printf("\n^=======================================^");
+
+    printf("\n Enter Size = ");
+    scanf("%d",&i);
+
+    printf("\n\n Pattern is => \n\n");
+
+    for( R = 1; R <= i; R++)
+    {
+        for( C = 1; C <= i; C++)
+        {
+            if(R + C >= i + 1)
+            {
+                printf(" * ");
+            }
+
+            else
+            {
+                printf("   ");
+            }
+
+        }
+        printf("\n");
+
+    }
+
+    printf("\n^=======================================^");
+
+    getch();
+    return 0;
+}
