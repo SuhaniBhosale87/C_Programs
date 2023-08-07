d0b264f6c10febd65c792a16f5e8bb51bb785a25
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-08-07 22:32:24 +0530

    Add files via upload

---
+++ Bitwise/01 Bitwise Using AND Operator.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0, Res = 0;
+
+    printf("\n Enter First Number = ");
+    scanf("%d",&No1);
+
+    printf("\n Enter Second Number = ");
+    scanf("%d",&No2);
+
+    printf("\n ****************************************");
+
+    Res = No1 & No2;
+
+    printf("\n Result => %d & %d = %d.",No1,No2,Res);
+
+    printf("\n ****************************************");
+
+    getch();
+    return 0;
+
+}
