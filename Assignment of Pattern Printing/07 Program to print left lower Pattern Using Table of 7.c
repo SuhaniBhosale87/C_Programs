e92a1dc49991340779a802e3bfa361364b0571e2
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-08-12 21:30:28 +0530

    Add files via upload

---
+++ Assignment of Pattern Printing/07 Program to print left lower Pattern Using Table of 7.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int R = 0, C = 0, i = 1,x = 0;
+
+    printf("\n Enter size For pattern => ");
+    scanf("%d",&x);
+
+    printf("============================== Pattern =============================\n");
+
+    for( R = 1, i = 7; R <= x; R++)
+    {
+        for( C = 1; C <= x; C++)
+        {
+            if(R >= C || R == C)
+            {
+               printf(" %3d ",i);
+               i = i+7;
+            }
+
+        }
+        printf("\n\n");
+
+    }
+
+    printf("***************************** Thanks *******************************\n");
+
+    getch();
+    return 0;
+}
