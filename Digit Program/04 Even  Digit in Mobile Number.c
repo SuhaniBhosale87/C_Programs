9cffca12fb6cb8b7b705d8e669132672ceb737bc
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-08-16 11:47:43 +0530

    Add files via upload

---
+++ Digit Program/04 Even  Digit in Mobile Number.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   long int Mob_No = 0,Temp = 0;
+
+   int Dig = 0,E_cnt= 0;
+
+    printf("\n Enter Your Mobile Number = ");
+    scanf("%ld",&Mob_No);
+
+    Temp = Mob_No;
+
+    while(Temp > 0)
+    {
+        Dig = Temp % 10;
+
+        if(Dig % 2 == 0 && Dig != 0 )
+        {
+            E_cnt++;
+        }
+        Temp = Temp / 10;
+
+    }
+
+    printf("\n Even Digits in Mobile No = %d",E_cnt);
+
+    getch();
+    return 0;
+}
+
