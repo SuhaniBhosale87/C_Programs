28e62ab0abdaab43407eae140cae91fc94e386b1
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-08-18 19:28:04 +0530

    Add files via upload

---
+++ Digit Program/07 Maximum Digit in Mobile Number.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   long int Mob_No = 0,Temp = 0;
+
+   int Dig = 0,Max= 0;
+
+    printf("\n Enter Your Mobile Number = ");
+    scanf("%ld",&Mob_No);
+
+    Temp = Mob_No;1
+
+    while(Temp > 0)
+    {
+        Dig = Temp % 10;
+
+        if(Dig > Max )
+        {
+            Max = Dig;
+        }
+        Temp = Temp / 10;
+
+    }
+
+    printf("\n Minimum Digits in Mobile Number is  = %d",Max);
+
+    getch();
+    return 0;
+}
+
