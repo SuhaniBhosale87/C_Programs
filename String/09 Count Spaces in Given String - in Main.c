ab1bc6181f6f5fca8b75265f38f25ef1b4d0c790
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-09-07 20:32:17 +0530

    Add files via upload

---
+++ String/09 Count Spaces in Given String - in Main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[20] = {'\0'};
+    int Sm_Cnt = 0,i = 0;
+
+    printf("\n Enter a String : ");
+    gets(cSrc);
+
+    while(cSrc[i] != '\0')
+    {
+        if(cSrc[i] >= 'a' && cSrc[i] <= 'z')
+        {
+            Sm_Cnt++;
+        }
+        i++;
+    }
+
+    printf("\n Count of Samll Letters in Given String is = %d ",Sm_Cnt);
+
+    getch();
+    return 0;
+}
