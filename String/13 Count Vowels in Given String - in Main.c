e0dab76a4b83658a1db6c5127c082dd878dd315d
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-10-03 20:00:34 +0530

    Add files via upload

---
+++ String/13 Count Vowels in Given String - in Main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[20] = {'\0'};
+    int v_Cnt = 0,i = 0;
+
+    printf("\n Enter a String : ");
+    gets(cSrc);
+
+    while(cSrc[i] != '\0')
+    {
+        if(cSrc[i] == 'a' ||cSrc[i] == 'e'||cSrc[i] == 'i'||cSrc[i] == 'o'||cSrc[i] == 'u'||cSrc[i] == 'A'||cSrc[i] == 'E'||cSrc[i] == 'I'||cSrc[i] == 'O'||cSrc[i] == 'U')
+        {
+            v_Cnt++;
+        }
+        i++;
+    }
+
+
+    printf("\n Count of Vowels in Given String is = %d ",v_Cnt);
+
+    getch();
+    return 0;
+}
