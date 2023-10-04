8452e7eec692bfb68f90cea6cd1696c1edd2b3f5
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-10-04 21:22:52 +0530

    Add files via upload

---
+++ String/13A Count White Spaces in Given String.c
@@ -0,0 +1,37 @@
+// strcpy()
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char FirstName[20] = "";
+    char MiddleName[20] = "";
+    char LastName[20] = "";
+    char Name[40] = "";
+
+    printf("\n Enter First Name = ");
+    gets(FirstName);
+
+    printf("\n Enter Middle Name = ");
+    gets(MiddleName);
+
+    printf("\n Enter Last Name = ");
+    gets(LastName);
+
+    getch();
+
+    strcat(Name, FirstName);
+    strcat(Name, " ");
+    strcat(Name, MiddleName);
+    strcat(Name, " ");
+    strcat(Name, LastName);
+
+    printf("\n\n First Name is = %s.",FirstName);
+    printf("\n\n Middle Name is = %s.",MiddleName);
+    printf("\n\n Last Name is = %s.",LastName);
+    printf("\n\n Full Name Generated is = %s.",Name);
+
+    getch();
+    return 0;
+}
