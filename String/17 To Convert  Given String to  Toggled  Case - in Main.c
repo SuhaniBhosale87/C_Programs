f323fca3785ec2e1c26bb6027ad84cd84dd44b31
SuhaniBhosale87 <137034254+SuhaniBhosale87@users.noreply.github.com>
2023-10-10 21:32:17 +0530

    Add files via upload

---
+++ String/17 To Convert  Given String to  Toggled  Case - in Main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[20] = {'\0'};
+    int i = 0;
+
+    printf("\n Enter a String : ");
+    gets(cSrc);
+
+    while(cSrc[i] != '\0')
+    {
+        if(cSrc[i] >= 'A' && cSrc[i] <= 'Z')
+        {
+            cSrc[i] = cSrc[i] + 32;
+        }
+        else if(cSrc[i] >= 'a' && cSrc[i] <= 'z')
+        {
+            cSrc[i] = cSrc[i] - 32;
+        }
+
+        i++;
+    }
+
+    printf("\n  Given String is After Capitalization = %s ",cSrc);
+
+    getch();
+    return 0;
+}
